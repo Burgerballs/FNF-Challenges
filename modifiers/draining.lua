05ed8cef54e704a83160a3ff36fc72e06be895bb
Burgerballs <107233412+Burgerballs@users.noreply.github.com>
2023-03-05 19:18:24 +0000

    Update draining.lua

--- modifiers/draining.lua
+++ modifiers/draining.lua
@@ -1,5 +1,5 @@
 function onUpdatePost(e)
   if getProperty('health') <= 0.05 then
-    setProperty('health', getProperty('health') - (e / 20))
+    setProperty('health', getProperty('health') - (e / 10))
   end
 end
