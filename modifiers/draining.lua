31690337338ce81b518d4859262e65b6a963c8bb
Burgerballs <107233412+Burgerballs@users.noreply.github.com>
2023-03-05 20:04:49 +0000

    Update draining.lua

--- modifiers/draining.lua
+++ modifiers/draining.lua
@@ -1,7 +1,7 @@
 function onUpdatePost(e)
   debugPrint('i work!!')
   local health = getHealth()
-  if health <= 0.05 then
-    addHealth(-(e * 0.03))
+  if health < 0.05 then
+    addHealth(-(e * 0.3))
   end
 end
