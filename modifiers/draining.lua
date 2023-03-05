301d8dedefa1b4bf4681f04bcb993ad2db10302e
Burgerballs <107233412+Burgerballs@users.noreply.github.com>
2023-03-05 20:14:58 +0000

    Update draining.lua

--- modifiers/draining.lua
+++ modifiers/draining.lua
@@ -1,6 +1,6 @@
-function opponentNoteHit()
+function opponentNoteHit(a,b,c,d)
   local health = getHealth()
-  if health > 0.16 then
+  if health > 0.16 and not d then
     addHealth(-(0.08))
   end
 end
